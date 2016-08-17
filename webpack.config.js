4be3254ed2904af8732ece4d6b7b3545f40d4583
Meillet Robin <robin.meillet@epitech.eu>
2016-08-18 01:02:14 +0200

    Fix webpack configuration for build
    When we use WebPack, we need to add the "resolve" (in configuration) object to choose files who can import by extension. Otherwhise, WebPack will import ALL files in bundle ... like WebPack himself or a "package.json" !

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var extractCSS = new ExtractTextPlugin('bundle.css');
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
+  resolve: {
+    extensions: ['', '.js', '.css'],
+    fallback: [path.join(__dirname, '../node_modules')],
+  },
   entry: {
     app: ['./src/app.js']
   },
   output: {
-      path: __dirname + '/dist',
-      filename: 'bundle.js',
-      publicPath: '/dist/',
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    publicPath: '/dist/',
   },
   module: {
     loaders: [
@@ -28,9 +33,9 @@ module.exports = {
     ]
   },
   plugins: [
-      extractCSS,
-      new webpack.optimize.UglifyJsPlugin({
-        comments: false
-      })
+    extractCSS,
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false
+    })
   ]
 };
